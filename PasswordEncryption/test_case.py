a02681d49b331f2890ca3c74e47c2e10c56de4bc
ZindalP <87191311+ZindalP@users.noreply.github.com>
2022-07-07 14:31:55 +0530

    Update test_case.py

--- PasswordEncryption/test_case.py
+++ PasswordEncryption/test_case.py
@@ -10,23 +10,22 @@ class HybridIOSTests(unittest.TestCase):
 
     # set up appium
     def setUp(self):
-        currentDate = datetime.now().strftime('%Y-%m-%d')
-        currentTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        caps = {}
+#         currentDate = datetime.now().strftime('%Y-%m-%d')
+#         currentTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#         caps = {}
         
-        caps['platformName'] = 'iOS'
-        caps['appium:app'] = 'storage:filename=SmokeTest.zip' # The filename of the mobile app
-        caps['appium:deviceName'] = 'iPhone 11 Simulator'
-        caps['appium:platformVersion'] = '15.0'
-        caps['sauce:options'] = {}
-        caps['sauce:options']['appiumVersion'] = '1.22.3'
-        caps['sauce:options']['build'] = 'Platform Configurator Build ' + currentDate
-        caps['sauce:options']['name'] = 'Platform Configurator Job ' + currentTime
-        caps['prerun'] = 'https://github.com/ZindalP/DemoTest/blob/main/config.sh'
+#         caps['platformName'] = 'iOS'
+#         caps['appium:app'] = 'storage:filename=SmokeTest.zip' # The filename of the mobile app
+#         caps['appium:deviceName'] = 'iPhone 11 Simulator'
+#         caps['appium:platformVersion'] = '15.0'
+#         caps['sauce:options'] = {}
+#         caps['sauce:options']['appiumVersion'] = '1.22.3'
+#         caps['sauce:options']['build'] = 'Platform Configurator Build ' + currentDate
+#         caps['sauce:options']['name'] = 'Platform Configurator Job ' + currentTime
 
-        url = 'https://oauth-jindalp-c7529:86220a08-6793-482f-bd86-6ccbca85ad72@ondemand.eu-central-1.saucelabs.com:443/wd/hub'
-#oauth-jindal.patel-3782a:01f7c8a5-53fa-4288-b91b-7767cd2b2218
-        self.driver=webdriver.Remote(url,caps)
+#         url = 'https://oauth-jindalp-c7529:86220a08-6793-482f-bd86-6ccbca85ad72@ondemand.eu-central-1.saucelabs.com:443/wd/hub'
+
+#         self.driver=webdriver.Remote(url,caps)
     
     def tearDown(self):
         sleep(1)
