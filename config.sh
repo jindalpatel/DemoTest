ca4db9c40cd106ce5f8cb58e5849756fca51cdfe
ZindalP <87191311+ZindalP@users.noreply.github.com>
2022-07-07 13:50:04 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "localhost:8080 maki2011.miso.saucelabs.com" >> /etc/hosts
+# echo "localhost:8080 maki2011.miso.saucelabs.com" >> /etc/hosts
+cd Desktop
+curl -0 https://github.com/ZindalP/DemoTest/blob/main/PasswordEncryption/test_case.py
+curl -0 https://github.com/ZindalP/DemoTest/blob/main/PasswordEncryption/Constant.py
+python3 test_case.py
