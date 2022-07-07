c13d407c6791b444daae89cc41beaa3a08f8b84f
ZindalP <87191311+ZindalP@users.noreply.github.com>
2022-07-07 15:42:13 +0530

    Update run.py

--- PasswordEncryption/run.py
+++ PasswordEncryption/run.py
@@ -16,10 +16,10 @@ def setUp():
     caps['sauce:options']['appiumVersion'] = '1.22.3'
     caps['sauce:options']['build'] = 'Platform Configurator Build ' + currentDate
     caps['sauce:options']['name'] = 'Platform Configurator Job ' + currentTime
-    caps['prerun'] = {
-        'executable' : 'https://github.com/ZindalP/DemoTest/blob/main/config.sh',
-        'background' : 'false'
-    }
+#     caps['prerun'] = {
+#         'executable' : 'https://github.com/ZindalP/DemoTest/blob/main/config.sh',
+#         'background' : 'false'
+#     }
     url = 'https://oauth-jindalp-c7529:86220a08-6793-482f-bd86-6ccbca85ad72@ondemand.eu-central-1.saucelabs.com:443/wd/hub'
     driver=webdriver.Remote(url,caps)
 
